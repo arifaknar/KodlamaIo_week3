19fed29fc0572e165b05cad7643d7b16bfd4a2d1
arifaknr <35777713+arifaknr@users.noreply.github.com>
2021-05-01 18:48:12 +0300

    Update StudentManager.java

--- src/StudentManager.java
+++ src/StudentManager.java
@@ -1,21 +1,21 @@
 
 public class StudentManager extends UserManager {
 	
-	public void SingUp(Student student) {
+	public void singUp(Student student) {
 		System.out.println(student.getFirstName()+" kodlama.io sitesine kaydýnýz baþarý ile yapýlmýþtýr.");
 	}
 	
-	public void RemoveAccount(Student student) {
+	public void removeAccount(Student student) {
 		System.out.println(student.getFirstName()+" kodlama.io sitesine kaydýnýz silinmiþtir.");
 	}
 	
-	public void JoinCources(Student student) {
+	public void joinCources(Student student) {
 		
 		System.out.println(student.getFirstName()+" "+" adlý öðrencinin "+student.getMyCourse()+
 		" adlý kursa kaydý baþarý ile yapýlmýþtýr.");
 	}
 	
-	public void RemoveCources(Student student) {
+	public void removeCources(Student student) {
 		
 		System.out.println(student.getFirstName()+" "+
 		" adlý öðrencinin+"+ student.getMyCourse() +" kursundan kaydý silinmiþtir.");
